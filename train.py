dbc7010e4cd11e75fbc047e177371f2d0dfa3313
fornacis-202 <77787902+fornacis-202@users.noreply.github.com>
2025-10-30 18:33:27 +0330

    Fix parameter access in TrainState creation

--- train.py
+++ train.py
@@ -162,7 +162,7 @@ def main(unused_argv):
   # Create the TrainState
   state = utils.TrainState.create(
       apply_fn=model.apply,
-      params=variables,
+      params=variables["params"],
       tx=optimizer_def)
   del optimizer_def, variables
   # --- END CHANGED BLOCK ---
